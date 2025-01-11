c333e76e36d6bd59e352c9c119dbb3ec61d6ff90
Adesina <damiiyi@gmail.com>
2025-01-11 02:36:40 +0100

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,6 @@ const io = new socketIo(server, {
 
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URL || "")
   .then(() => console.log("Connected to MongoDB"))
@@ -44,7 +43,7 @@ let jobQueue: { a: number; b: number }[] = [];
 app.post("/compute", (req, res) => {
   const { a, b } = req.body;
   jobQueue.push({ a, b });
-  if (jobQueue.length === 1) processJobs(); // Start processing jobs only if it's the first job
+  if (jobQueue.length === 1) processJobs();
   res.status(200).send({ message: "Computation started" });
 });
 
@@ -64,33 +63,21 @@ const processJobs = () => {
   jobs.forEach((item, index) => {
     setTimeout(async () => {
       const result = item.func(job.a, job.b);
-
-      // Had bug in production where the first job doesn't work. Properly due to the frontend not establishing connection early. Emit result only when the socket connection is ready
-      if (io.engine.clientsCount > 0) {
-        io.emit("result", {
-          jobType: item.type,
-          result,
-          progress: ((index + 1) / jobs.length) * 100,
-        });
-      }
+      io.emit("result", {
+        jobType: item.type,
+        result,
+        progress: 25,
+      });
 
       if (item.type === "A+B") results.additionResult = result;
       if (item.type === "A-B") results.subtractionResult = result;
       if (item.type === "A*B") results.multiplyResult = result;
       if (item.type === "A/B") results.divisionResult = result;
 
-      if (index === jobs.length - 1) {
-        try {
-          const newJob = new JobModel(results);
-          await newJob.save();
-          console.log("Job saved to MongoDB:", results);
-        } catch (err) {
-          console.error("Error saving job to MongoDB:", err);
-        }
-
-        if (jobQueue.length) processJobs(); // Process next job in the queue
-      }
-    }, 3000 * index); // Simulate job processing delay
+      const newJob = new JobModel(results);
+      await newJob.save();
+      console.log("Job saved to MongoDB:", results);
+    }, 3000 * index);
   });
 };
 
